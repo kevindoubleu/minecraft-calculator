b7c51d030d2cf5a21d4263a5db4088353247e63a
Kevin Wibamanto <kevin.wibamanto@gopay.co.id>
2025-01-29 11:04:30 +0700

    add incrementing global var

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import tkinter
+from tkinter import ttk
+
+iron_needed: int = 0
 
 def main(window: tkinter.Tk):
     window.title("minecraft calculator v1")
+    window.geometry("854x480")
 
-    add_label(window, 'What items do you want to make?')
+    frame = ttk.Frame(window, padding=10)
+    frame.grid()
+
+    label = ttk.Label(frame, text="What items do you want to make?")
+    label.grid(row=0, column=0)
+
+    btn = ttk.Button(frame, text="Hopper", command=lambda: add_iron(5))
+    btn.grid(row=1, column=0)
+    btn = ttk.Button(frame, text="Anvil", command=lambda: add_iron(30))
+    btn.grid(row=1, column=1)
 
     window.mainloop()
 
-def add_label(window, text):
-    text_label = tkinter.Label(window, text=text)
-    text_label.pack()
+def add_iron(amount: int):
+    global iron_needed
+    iron_needed += amount
+    print(f"you need {iron_needed} iron ingots")
 
 main(tkinter.Tk())
